7c4a67a7eb9c774a0edc6f81efad07461bc7d704
Je <54561597+Jaeson-gram@users.noreply.github.com>
2024-03-17 02:31:26 +0100

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿using Papaya.Models;
+
+namespace Papaya
+{
+    public partial class MainPage : ContentPage
+    {
+        //int count = 0;
+
+        public List<FruitInformation> PapayaInfo { get; set; }
+
+        public MainPage()
+        {
+
+
+            NavigationPage.SetHasNavigationBar(this, false);
+
+            InitializeComponent();
+
+
+            PapayaInfo = new List<FruitInformation>()
+            {
+                new FruitInformation
+                {
+                    Micronutrient = "Vitamin C",
+                    Percentage = 101
+                },
+
+                new FruitInformation
+                {
+                    Micronutrient = "Vitamin A",
+                    Percentage = 19
+                },
+
+                new FruitInformation
+                {
+                    Micronutrient = "Calcium",
+                    Percentage = 2
+                },
+
+                new FruitInformation
+                {
+                    Micronutrient = "Magnesium",
+                    Percentage = 5
+                },
+
+                new FruitInformation
+                {
+                    Micronutrient = "Potassium",
+                    Percentage = 5
+                }
+            };
+
+            BindingContext = this;
+        }
+
+
+    }
+
+}
---
+++ MauiProgram.cs
@@ -0,0 +1,52 @@
+﻿using CommunityToolkit.Maui;
+using Microsoft.Extensions.Logging;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.LifecycleEvents;
+using System.Runtime.Intrinsics.X86;
+using System.Threading.Channels;
+using Xe.AcrylicView;
+
+namespace Papaya
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+            .UseMauiApp<App>()
+            .UseMauiCommunityToolkit()
+                .UseAcrylicView()
+                .ConfigureLifecycleEvents(events =>
+                 {
+#if ANDROID
+              
+                events.AddAndroid(android => android.OnCreate((activity, bundle) => MakeStatusBarTranslucent(activity)));
+      
+                static void MakeStatusBarTranslucent(Android.App.Activity activity)
+                {
+             
+                activity.Window.SetNavigationBarColor(Android.Graphics.Color.ParseColor("#FCBC03"));
+
+                }
+#endif
+                 })
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("Roboto-Regular.ttf", "Regular");
+                    fonts.AddFont("Roboto-Bold.ttf", "Bold");
+
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
+
+//#if DEBUG
+//    		builder.Logging.AddDebug();
+//#endif
+
+            return builder.Build();
+        }
+
+
+    }
+}
