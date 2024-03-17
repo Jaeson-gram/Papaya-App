b8328334bd9dddf180019fa813e79c3bc90e2014
Je <54561597+Jaeson-gram@users.noreply.github.com>
2024-03-17 02:44:29 +0100

    Add files via upload

---
+++ Models/FruitInformation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Papaya.Models;
+
+    public class FruitInformation
+    {
+        public required String Micronutrient { get; set; }
+
+        public int Percentage { get; set; }
+    }
+
---
+++ Platforms/Android/MainActivity.cs
@@ -0,0 +1,18 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.OS;
+
+namespace Papaya
+{
+    [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+    public class MainActivity : MauiAppCompatActivity
+    {
+        //protected override void OnCreate(Bundle? savedInstanceState)
+        //{
+        //    Window.SetNavigationBarColor(Android.Graphics.Color.ParseColor("000000")); 
+
+        //    base.OnCreate(savedInstanceState);
+        //    base.OnCreate(savedInstanceState);
+        //}
+    }
+}
